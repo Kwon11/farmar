73985fe5388a2b1a694a5c68fda73c9cba7227a8
Yuehan Duan <yuehanduan@yahoo.com>
2018-06-10 17:36:10 -0700

    Add comma

--- .env.js
+++ .env.js
@@ -1,6 +1,6 @@
 module.exports = {
   GMAIL_USERNAME: 'GrandTophamRanch',
   GMAIL_PASSWORD: 'TelAviv2020',
-  GMAIL_RECEIVER: 'shlomo.amar@gtragsupplies.com'
+  GMAIL_RECEIVER: 'shlomo.amar@gtragsupplies.com',
   GMAIL_RECEIVER2: 'Yoram.kesten@gtragsupplies.com'
 };
